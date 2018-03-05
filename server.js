617e7b007081eae472b48635ad455a8b0ab60629
codigomorton <codigomorton@gmail.com>
2018-03-05 02:06:16 -0300

    second push

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.set('port', process.env.PORT || 8080);
+app.set('address', process.env.IP || '127.0.0.1');
+app.use(express.static('app'));
+
+app.get('/', function(req, res) {
+  res.sendFile('index.html');
+});
+
+http.listen(app.get('port'), app.get('address'), function(){
+  console.log('Express server listening on  IP: ' + app.get('address') + ' and port ' + app.get('port'));
+});
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  socket.on('chat message', function(msg) {
+  	io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
+  socket.on('card played', function(msg) {
+  	io.emit('card played', msg);
+    console.log('card: ' + msg);
+  });
+});
