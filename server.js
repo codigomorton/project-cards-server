6f10bd7cbb773ef51553a1be59700db68c7ee849
codigomorton <codigomorton@gmail.com>
2018-03-05 02:15:32 -0300

    dfdf

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+var http = require('http').Server(express);
 var io = require('socket.io')(http);
 
-app.set('port', process.env.PORT || 8080);
-app.set('address', process.env.IP || '127.0.0.1');
-app.use(express.static('app'));
-
-app.get('/', function(req, res) {
-  console.log('heeeey');
-  res.render('index.html');
-});
-
-http.listen(app.get('port'), app.get('address'), function(){
-  console.log('Express server listening on  IP: ' + app.get('address') + ' and port ' + app.get('port'));
-});
+express()
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index.html'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 io.on('connection', function(socket) {
   console.log('a user connected');
