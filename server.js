a76dba0d43d53664e4157b41b1a39662d0ef552b
codigomorton <codigomorton@gmail.com>
2018-03-05 02:20:53 -0300

    dfsdfds

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var http = require('http').Server(express);
 var io = require('socket.io')(http);
 
 express()
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('public/index.html'))
+  .use(express.static(path.join(__dirname, 'public')))
+  .get('/', (req, res) => res.sendFile('public/index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 io.on('connection', function(socket) {
