0fedd7359a2cc95ce8cffda736636ee1017d3c52
codigomorton <codigomorton@gmail.com>
2018-03-05 03:24:06 -0300

    second push

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
 const PORT = process.env.PORT || 5000
-var http = require('http').Server(express);
-const io = require('socket.io')(http);
 
-express()
+const server = express()
   .use(express.static(path.join(__dirname, 'public')))
   .get('/', (req, res) => res.sendFile('public/index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const io = socketIO(server);
 
 io.on('connection', function(socket) {
   console.log('a user connected');
