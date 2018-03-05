cc159fbfcec628194fd702a14bbadf5c29e317e5
codigomorton <codigomorton@gmail.com>
2018-03-05 03:18:49 -0300

    second push

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 var http = require('http').Server(express);
-const io = socketIO(express);
+const io = require('socket.io')(http);
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
